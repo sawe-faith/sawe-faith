b480fe1816798ef23087133b46cdf4d261061bad
sawe-faith <faithsawe23@gmail.com>
2025-01-13 23:19:18 +0300

    done

--- isuggest/app/page.tsx
+++ isuggest/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import Navbar from './components/Navbar.jsx'
 import { SiWorkplace } from "react-icons/si";
 import Link from 'next/link.js';
 import { FaChevronDown } from "react-icons/fa";
